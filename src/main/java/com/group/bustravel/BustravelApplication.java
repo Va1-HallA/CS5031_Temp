03950ab2e36be718aad633da03e596bb2da97f60
190003505 <random@st-andrews.ac.uk>
2023-02-21 05:23:00 +0000

    initial project, with pom.xml configured to use JUnit 5.

---
+++ src/main/java/com/group/bustravel/BustravelApplication.java
@@ -0,0 +1,13 @@
+package com.group.bustravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BustravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BustravelApplication.class, args);
+	}
+
+}
